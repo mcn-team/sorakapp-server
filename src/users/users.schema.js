7bacc8c5a970116fb18480e82c8e4b9f2a3de2cb
Alexandre Cardoso <alexandre.cardoso.dev@gmail.com>
2019-12-14 16:35:20 +0100

    modified schema fields
    added required: true

--- src/users/users.schema.js
+++ src/users/users.schema.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: String,
-    password: String,
-    role: String,
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
 }, { timestamps: true });
 
 mongoose.model('User', userSchema);
