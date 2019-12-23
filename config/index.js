d4ff44126a3098f9d90a1bfa6ad8b58e3d8556d0
Matthias Brunet <brunetmatthias@outlook.com>
2019-12-23 16:14:22 +0100

    rework config and add validation

--- config/index.js
+++ config/index.js
@@ -1,4 +1,30 @@
-const env = process.env.NODE_ENV || 'local';
-const config = require(`../config/config.${env}.json`);
+const Joi = require('@hapi/joi');
 
-exports.config = config;
+const CONFIG = require('./config.json');
+
+const MIN_DB_NAME_LENGTH = 4;
+const MAX_DB_NAME_LENGTH = 24;
+const MIN_PORT_NUMBER = 1025;
+const MAX_PORT_NUMBER = 65534;
+
+const configSchema = Joi.object({
+    DB_URL: Joi
+        .string()
+        .uri({ scheme: [ 'mongodb' ] })
+        .required(),
+    DB_NAME: Joi
+        .string()
+        .min(MIN_DB_NAME_LENGTH)
+        .max(MAX_DB_NAME_LENGTH)
+        .regex(/^[a-zA-Z]+$/)
+        .required(),
+    PORT: Joi
+        .number()
+        .min(MIN_PORT_NUMBER)
+        .max(MAX_PORT_NUMBER)
+        .required()
+});
+
+Joi.assert(CONFIG, configSchema);
+
+exports.config = CONFIG;
