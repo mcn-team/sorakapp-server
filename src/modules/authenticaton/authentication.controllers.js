6acd7b1ce9a8cebfdb953f2e9a876c6a099bd940
Alexandre Cardoso <alexandre.cardoso.dev@gmail.com>
2019-12-26 14:11:59 +0100

    updated authentication

--- src/modules/authenticaton/authentication.controllers.js
+++ src/modules/authenticaton/authentication.controllers.js
@@ -5,21 +5,25 @@ const { errorHTTPHandler } = require('../../utils/error-http-handler.utils');
 const { HTTP_UNAUTHORIZED, HTTP_INTERNAL_SERVER_ERROR } = require('../../constants/http.constants');
 
 async function authenticate(req, res) {
-    try {
-        const logs = {
-            username: req.body.username,
-            password: req.body.password
-        };
-        const token = await services.authenticate(logs);
+    const logs = {
+        username: req.body.username,
+        password: req.body.password
+    };
+
+    //authenticate user
+    const token = await services.authenticate(logs);
 
     if (!token) {
         throw errorHTTPHandler(HTTP_UNAUTHORIZED, 'login error');
     }
+
+    res.send({ result: token }); //authentication success
 }
 
 async function register(req, res) {
     try {
         const data = req.body;
+
         const token = await services.register(data);
 
         res.send({ token: token }); //authentication success
